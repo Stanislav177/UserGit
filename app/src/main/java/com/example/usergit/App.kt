792f8b7ac5845c69a62b1f5f02ebaf52873c53cd
Stanislav <stas.177@mail.ru>
2022-06-15 11:43:58 +0300

    добавил имя app файла в манифест

--- app/src/main/java/com/example/usergit/App.kt
+++ app/src/main/java/com/example/usergit/App.kt
@@ -7,7 +7,7 @@ import com.example.usergit.domain.repos.RepoUsers
 
 class App : Application() {
 
-    val userRepoUsers: RepoUsers by lazy { APIRepoUsersImpl() }
+    val userRepoUsers: RepoUsers by lazy { LocalRepoUsersImpl() }
 
 }
 
