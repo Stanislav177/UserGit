c053f856f5b2c7eddb27080ac40fe3249dcc086b
Stanislav <stas.177@mail.ru>
2022-06-19 12:24:46 +0300

    создал новую активити для детального отображения информации о пользователе

---
+++ app/src/main/java/com/example/usergit/DetailingUserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.usergit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailingUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailing_user)
+    }
+}
