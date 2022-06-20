2d3109fe4ae3c22ae8abc44d248d59b24c06d593
Stanislav <stas.177@mail.ru>
2022-06-20 12:04:53 +0300

    2-ое дз готово

--- app/src/main/java/com/example/usergit/ui/detailingUser/DetailingUserActivity.kt
+++ app/src/main/java/com/example/usergit/ui/detailingUser/DetailingUserActivity.kt
@@ -1,10 +1,11 @@
 package com.example.usergit.ui.detailingUser
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
 import com.example.usergit.databinding.ActivityDetailingUserBinding
-import com.example.usergit.domain.UserDetailing
 import com.example.usergit.domain.UserEntity
 
 class DetailingUserActivity : AppCompatActivity() {
@@ -13,26 +14,31 @@ class DetailingUserActivity : AppCompatActivity() {
 
     private var detailingUserData: UserEntity? = null
 
+    private var uri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailingUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         detailingUserData =
-            intent.getParcelableExtra("KEY") ?: UserEntity(0,
-                " ",
-                " ",
-                " ",
-                UserDetailing(" ", " ", 0))
-
-
-
+            intent.getParcelableExtra("KEY")
         displayUserEntity()
+
+        onOpenPageUser()
+    }
+
+    private fun onOpenPageUser() {
+        binding.onClickWebUser.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
     private fun displayUserEntity() {
         detailingUserData?.let {
             with(binding) {
+                uri = Uri.parse(it.urlUser)
                 nameUserDetailingActivity.text = it.detailing.name
                 loginUserDetailingActivity.text = it.nickname
                 idUserDetailingActivity.text = it.id.toString()
