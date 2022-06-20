fe7262a7eadb4ed44b0b98cc90c22efcb00e1522
Stanislav <stas.177@mail.ru>
2022-06-20 11:02:12 +0300

    добавил ландшафтную ориентацию детального отображения пользователя

--- app/src/main/java/com/example/usergit/ui/detailingUser/DetailingUserActivity.kt
+++ app/src/main/java/com/example/usergit/ui/detailingUser/DetailingUserActivity.kt
@@ -4,22 +4,42 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
 import com.example.usergit.databinding.ActivityDetailingUserBinding
+import com.example.usergit.domain.UserDetailing
 import com.example.usergit.domain.UserEntity
 
 class DetailingUserActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityDetailingUserBinding
+
+    private var detailingUserData: UserEntity? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailingUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val user = intent.getParcelableExtra<UserEntity>("KEY")
-        binding.nameUserDetailingActivity.text = user!!.detailing.name
-        binding.loginUserDetailingActivity.text = user.nickname
-        binding.idUserDetailingActivity.text = user.id.toString()
-        binding.locationUserDetailingActivity.text = user.detailing.location
-        binding.publicRepoUserDetailingActivity.text = user.detailing.publicRepos.toString()
-        binding.userAvatarDetailingActivity.load(user.urlAvatar)
+        detailingUserData =
+            intent.getParcelableExtra("KEY") ?: UserEntity(0,
+                " ",
+                " ",
+                " ",
+                UserDetailing(" ", " ", 0))
+
+
+
+        displayUserEntity()
+    }
+
+    private fun displayUserEntity() {
+        detailingUserData?.let {
+            with(binding) {
+                nameUserDetailingActivity.text = it.detailing.name
+                loginUserDetailingActivity.text = it.nickname
+                idUserDetailingActivity.text = it.id.toString()
+                locationUserDetailingActivity.text = it.detailing.location
+                publicRepoUserDetailingActivity.text = it.detailing.publicRepos.toString()
+                userAvatarDetailingActivity.load(it.urlAvatar)
+            }
+        }
     }
 }
